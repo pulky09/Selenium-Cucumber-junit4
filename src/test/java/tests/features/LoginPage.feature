9ee6c92d7ef1d0d754feedf3a8f1be671bb02f91
pulky09 <plkt.bhatia@gmail.com>
2023-08-10 19:13:25 -0300

    Updated feature files and steps

--- src/test/java/tests/features/LoginPage.feature
+++ src/test/java/tests/features/LoginPage.feature
@@ -3,5 +3,5 @@ Feature: Login functionality
 
     Scenario: Login functionality
       Given I want to navigate to the home page
-      When I want to login using my "hello778542@gmail.com" and "HelloWorld123"
+      When I want to login using my username "hello778542@gmail.com" and password "HelloWorld123"
       Then I should be on the customer login page
--- src/test/java/tests/steps/LoginSteps.java
+++ src/test/java/tests/steps/LoginSteps.java
@@ -2,9 +2,9 @@ package tests.steps;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.example.HomePage;
 import org.example.LoginPage;
-import org.openqa.selenium.WebDriver;
 import tests.hooks.CucumberHooks;
 import static org.junit.Assert.*;
 
@@ -18,7 +18,6 @@ public class LoginSteps {
                 this.hooks = hooks;
         }
 
-
         @Given("I want to navigate to the home page")
         public void i_want_to_navigate_to_the_home_page(){
                 HomePage homePage = new HomePage(hooks.getDriver());
@@ -26,16 +25,15 @@ public class LoginSteps {
                 loginPage = homePage.clickSignInLink();
         }
 
-        @Given("I want to login using my {string} and {string}")
-        public void iWantToLoginUsingMyAnd(String userName, String password) {
-              loginPage.login(userName,password);
+        @When("I want to login using my username {string} and password {string}")
+        public void iWantToLoginUsingMyUsernameAndPassword(String userName, String password) {
+                loginPage.login(userName,password);
         }
 
         @Then("I should be on the customer login page")
         public void iShouldBeOnTheCustomerLoginPage() {
                 assertEquals("Home Page",hooks.getDriver().getTitle());
         }
-
 
 }
 
